ff70d7d1e5d0ef0a10721c139b05fd583a562097
jaweria332 <hiyamughal03@gmail.com>
2021-05-15 01:31:53 +0500

    Update HandTrackingMin.py

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -21,6 +21,8 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLmr, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
