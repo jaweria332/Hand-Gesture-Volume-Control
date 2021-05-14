96545de027def38bf0dcd0ea7776167043a6d098
jaweria332 <hiyamughal03@gmail.com>
2021-05-15 01:30:11 +0500

    Adding HandTrackingMin.py

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,10 +5,28 @@ import time
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     ret, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hands.process(imgRGB)
+    if result.multi_hand_landmarks:
+        for handLmr in result.multi_hand_landmarks:
+            for id, lm in enumerate(handLmr.landmark):
+                # To get pixel value, we need to multiply x and y-coordinates of landmark(lm) with the heiht and widht
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+            mpDraw.draw_landmarks(img, handLmr, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
     cv2.imshow("Video Stream", img)
     cv2.waitKey(1)
